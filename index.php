174f65553f4cb8abb26eb8b00e3225abc86071d4
Alex Vong <avong003@gmail.com>
2022-09-29 08:35:33 -0700

    created an index.php and linked header

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+
+
+<html lang="en">
+	<?php include('head.php') ?>
+
+	<body>
+		<header>
+			<?php include('masthead.php') ?>
+		</header>
+
+		<main>
+			<inner-column>
+				<h1>Hello World</h1>
+			</inner-column>
+			
+		</main>
+
+		<footer>
+			<?php include('footer.php') ?>
+		</footer>
+
+		<script src="script.js"></script>
+
+
+	</body>
+	
+
+</html>
