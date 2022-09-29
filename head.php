80a312017e1156298595887cef8bc1853d1f8dcc
Alex Vong <avong003@gmail.com>
2022-09-29 08:35:52 -0700

    created head.php with meta data and linking styles

---
+++ head.php
@@ -0,0 +1,20 @@
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Huddle | Build The Community Your Fans Will Love</title>
+		<meta
+			name="og:description"
+			content="Build The Community Your Fans Will Love"
+		/>
+	
+		<meta property="og:title" content="Huddle re-imagines the way we build communities. Create connections with your users as your engage in genuine disuccions." />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="[[meta-image-here]]"  />
+		<meta property="og:image:type" content="[[meta-image-here]]]" />
+		<link rel="shortcut icon" type="image/jpg" href=[[]] />
+
+		<link rel='stylesheet' href='css/style.css'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
