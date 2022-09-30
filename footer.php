64c0cdffb67f2cbf6247073d4253407f7daa841d
Alex Vong <avong003@gmail.com>
2022-09-29 22:06:21 -0700

    created footed with subsections on main info, faqs, and social media handles

---
+++ footer.php
@@ -0,0 +1,55 @@
+<section class="footer-container">
+	<section class="footer-main-stuff">
+		<picture>
+			<img src="images/logo-footer.svg" alt="footer-company-logo" class="logo">
+		</picture>
+
+		<div class="location contact">
+			<picture>
+				<img src="images/icon-location.svg" alt="footer-company-logo">
+			</picture>
+
+			<p class="body-copy">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta dolorum sunt voluptas ratione dolorem sed voluptatum iure modi numquam nulla.</p>
+		</div>
+
+		<div class="email contact">
+			<picture>
+				<img src="images/icon-phone.svg" alt="footer-company-number">
+			</picture>
+
+			<a href="tel:+1234567891" class="body-copy">+123-456-7891</a>
+		</div>
+
+		<div class="phone-number contact">
+			<picture>
+				<img src="images/icon-email.svg" alt="footer-company-number">
+			</picture>
+
+			<a href="mailto:alexvongdev@gmail.com" class="body-copy">alexvongdev@gmail.com</a>
+		</div>
+	</section>
+
+	<section class="footer-facts-stuff">
+		<ul class="company-info">
+			<li class="body-copy">About Us</li>
+			<li class="body-copy">What We Do</li>
+			<li class="body-copy">FAQ</li>
+			<li class="body-copy">Careers</li>
+			<li class="body-copy">Blog</li>
+			<li class="body-copy">Contact Us</li>
+		</ul>
+	</section>
+
+
+	<section class="social-media-stuff">
+		<div class="sm-handles">
+			<i class="fa-brands fa-facebook-f"></i>
+			<i class="fa-brands fa-twitter"></i>
+			<i class="fa-brands fa-instagram"></i>
+		</div>
+
+		<div class="copy-right">
+			<p class="link-copy">&#169; Copyright 2022 Huddle. All Rights reserved.</p>
+		</div>
+	</section>
+</section>
