81ea9958c45b20915fa408ef71e87e9a54d057b2
Alex Vong <avong003@gmail.com>
2022-09-29 22:07:07 -0700

    created masthead php

---
+++ masthead.php
@@ -0,0 +1,12 @@
+<masthead>
+
+	<section class="masthead-logo">
+		<picture>
+			<img src="images/logo.svg" alt="huddle-company-logo">
+		</picture>
+	</section>
+
+	<section class="try-it-free">
+		<a href="" class="btn body-copy">Try It Free</a>
+	</section>
+</masthead>
