0c377e4265a33cdd922f33c91a406aaacf7b33ff
Alex Vong <avong003@gmail.com>
2022-09-29 22:07:00 -0700

    created landing php

---
+++ landing.php
@@ -0,0 +1,12 @@
+<section class="landing-container">
+	<section class="landing-text">
+		<h2 class="heading">Build the Community Your Fans Will Love</h2>
+		<p class="body-copy">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine&nbsp;discussion.</p>
+		<a href="" class="btn btn-pink link-copy">Get Started For Free</a>
+	</section>
+	<section class="landing-img">
+		<picture>
+			<img src="images/illustration-mockups.svg" alt="background-image-mobile">
+		</picture>
+	</section>
+</section>
