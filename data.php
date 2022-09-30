c9b940b4c133bde5be20ed5cd72992774fa90e71
Alex Vong <avong003@gmail.com>
2022-09-29 22:06:01 -0700

    created data php to store info on cards

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$cardData =[
+		[
+		"image" => "images/illustration-grow-together.svg",
+		"heading" => "Grow Together",
+		"description" => "Generate meaningful discussions with your audience and build a stronger, loyal community. Think of the insightful coversations you miss out on with a feedback form",
+		],
+
+		[
+		"image" => "images/illustration-flowing-conversation.svg",
+		"heading" => "Flowing Conversations",
+		"description" => "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just in-time loading for a more antural flow",
+		],
+
+		[
+		"image" => "images/illustration-your-users.svg",
+		"heading" => "Your Users",
+		"description" => "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed into your app, your users can start chatting immediately",
+		],
+
+
+	];
+
+
+ ?>
