300fd02417d354105a471005a6e0f0f58c4c790d
Alex Vong <avong003@gmail.com>
2022-09-29 22:06:52 -0700

    created index and added php components via include

--- index.php
+++ index.php
@@ -7,18 +7,41 @@
 
 	<body>
 		<header>
-			<?php include('masthead.php') ?>
-		</header>
-
-		<main>
 			<inner-column>
-				<h1>Hello World</h1>
+				<?php include('masthead.php') ?>
 			</inner-column>
+		</header>
+		<main>
+			<section class="landing-page">
+				<inner-column>
+					<?php include('landing.php') ?>
+				</inner-column>
+			</section>
+
+			<section class="card-grid">
+				<inner-column>
+					<?php include('card-grid.php') ?>
+				</inner-column>
+			</section>
+
+			<section class="cta">
+				<inner-column>
+
+					<section class="cta-container">
+						<h3 class="heading">Ready To Build Your Community?</h3>
+
+						<a href="" class="btn btn-pink body-copy">Get Started for Free</a>
+					</section>
+					
+				</inner-column>
+			</section>
 			
 		</main>
 
 		<footer>
-			<?php include('footer.php') ?>
+			<inner-column>
+				<?php include('footer.php') ?>
+			</inner-column>
 		</footer>
 
 		<script src="script.js"></script>
