0a5f05211baef11dc02eec5d7c289e868a3d0503
Alex Vong <avong003@gmail.com>
2022-09-30 09:46:57 -0700

    added container for card text in card grid

--- card-grid.php
+++ card-grid.php
@@ -9,11 +9,12 @@
 				<img src="<?=$data["image"]?>" alt="">
 			</picture>
 
-			<h3 class="heading"><?=$data["heading"]?></h3>
-			<p class="body-copy"><?=$data["description"]?></p>
+			<div class="card-text">
+				<h3 class="heading"><?=$data["heading"]?></h3>
+				<p class="body-copy"><?=$data["description"]?>
+			</div>	
+			</p>
 		</card>
 	<?php } ?>
-
-
 
 </section>
